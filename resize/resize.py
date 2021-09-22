add30c850d98c84fca618ce977e5e4340db06683
Dylanbron <13438101705@163.com>
2021-09-22 18:32:28 +0800

    nearest resize

--- resize/resize.py
+++ resize/resize.py
@@ -33,11 +33,32 @@ def bilinear_resize(src,size=(0,0),ratio=0):
     
     return dst
 
+def nearest_resize(src,size=(0,0),ratio=0):
+    h,w=src.shape[:2]
+    if ratio!=0:
+        dsth, dstw = h*ratio,w*ratio
+    elif size!=(0,0):
+        dsth, dstw = size[1],size[0]
+    else:
+        print("size and ratio error")
+        raise ValueError
+    src1 = src.copy()
+    src1 = np.pad(src1,((0,1),(0,1),(0,0)),'constant')
+    dst = np.zeros((dsth,dstw,3),dtype=np.uint8)
+    for h_idx in range(dsth):
+        for w_idx in range(dstw):
+            src_h_idx = int((h_idx+1)*h/dsth-1)
+            src_w_idx = int((w_idx+1)*w/dstw-1)
+            for c in range(3):
+                dst[h_idx,w_idx,c]=src1[src_h_idx,src_w_idx,c]
+    return dst
+
+
 import cv2
 
 img = cv2.imread("/Users/shenyeqing/file/repo/DIPlearn/images/rocket.png")
-img_resize = bilinear_resize(img,ratio=2)
-cv2.imwrite("/Users/shenyeqing/file/repo/DIPlearn/images/rocket_biresize.png",img_resize)
+img_resize = nearest_resize(img,ratio=2)
+cv2.imwrite("/Users/shenyeqing/file/repo/DIPlearn/images/rocket_nearreisze.png",img_resize)
 
 
 
